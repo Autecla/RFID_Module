d798f1ca6767a258e54bc6f59d8427adf71a2080
lbandeira <lbms@cin.ufpe.br>
2018-10-31 15:01:55 -0300

    Ajustes de envios pela serial

--- LED_RFID/LED_RFID.ino
+++ LED_RFID/LED_RFID.ino
@@ -180,7 +180,6 @@ void loop() {
    Helper routine to dump a byte array as hex values to Serial.
 
 */
-
 void dump_byte_array(byte * buffer, byte bufferSize) {
   for (byte i = 0; i < bufferSize; i++) {
     Serial.print(buffer[i] < 0x10 ? "0" : "");
